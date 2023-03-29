68f585bc220b58e2503988e615ae01db9a412877
8wondrs <8wondrs@gmail.com>
2023-03-29 21:35:28 +0300

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem(CURRENT_TIME, event.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
