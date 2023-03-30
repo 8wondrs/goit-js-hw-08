b90bb6fba16d2b18bbab605a7e119ad835b4e8b4
8wondrs <8wondrs@gmail.com>
2023-03-30 20:13:33 +0300

    add task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('[name="email"]');
+const messageInput = form.querySelector('[name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const saveFormState = throttle(() => {
+  const feedbackFormState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormState));
+}, 500);
+
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+
+window.addEventListener('load', () => {
+  const feedbackFormState = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (feedbackFormState) {
+    emailInput.value = feedbackFormState.email;
+    messageInput.value = feedbackFormState.message;
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const feedbackFormState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  if (emailInput.value === '' || messageInput === '') {
+    return alert('Будь ласка, заповніть всі поля');
+  }
+
+  console.log(feedbackFormState);
+
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+});
